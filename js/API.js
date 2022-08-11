2578ae9ae0c3f4bdbd2f865adba9fb583c3c6a9f
vicpack <vicmoze95@gmail.com>
2022-08-10 22:07:11 -0500

    Primeros pasos para consumir la API Free Meal

---
+++ js/API.js
@@ -0,0 +1,20 @@
+const API_URL = 'https://www.themealdb.com';
+
+const xhr = new XMLHttpRequest();
+
+function onRequestHandler() {
+    if(this.readyState === 4 && this.status === 200){
+        //0 = UNSET no se ha llamado al metodo open
+        //1 = OPENED , se ha llamado al metodo opened
+        //2 = HEADERS_RECEIVED, se está llamando al metodo send()
+        //3 = LOADING, esta cargando, es decir, esta recibiendo la respuesta
+        //4 = DONE , se ha completado la operacion del
+        //console.log(this.response);
+        const data = JSON.parse(this.response);
+        console.log(data);
+    }
+}
+
+xhr.addEventListener("load", onRequestHandler);
+xhr.open('GET',`${API_URL}/api/json/v1/1/random.php`);
+xhr.send();
