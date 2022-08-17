7ec21b34a89dbefc1528e3fbdad7d6b075e5c770
Eder MX <95453920+eder-mx@users.noreply.github.com>
2022-08-16 18:15:06 -0600

    Eder (#4)
    * Iniciando el repositorio con Boostrap 5 y folders
    
    * Se agrego Webpack
    
    * Repositorio actualizado con Babel
    
    * Se agrego Input buscador por nombre

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+      bundle: './src/js/API.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Recetario BEDU',
+            filename: 'index.html',
+            template: './src/app/index.html',
+            hash: true,
+        })
+    ],
+    // Modulos para Estilos CSS, Babel e Imagenes
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+          },
+          {
+            test: /\.js$/,
+            use:{
+              loader: 'babel-loader',
+              options:{
+                presets: ['@babel-preset-env']
+              }
+            }
+          },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'src/img'
+          }
+    ],},
+
+
+    // Server
+    devServer: {
+        static: {
+          directory: path.resolve(__dirname, 'dist'),
+        },
+        port: 8080,
+        open: true,
+        hot: true,
+      }
+}
