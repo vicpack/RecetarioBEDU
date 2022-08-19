d57f2b0df6e3537080fc9475e7861ced49881b39
Dionicio <dioniciomatamacias@gmail.com>
2022-08-19 14:24:19 -0500

    en archivo API.js expuse la variable 'data' al scope global, para luego poder imprimirla con 'function print'

--- src/js/API.js
+++ src/js/API.js
@@ -1,24 +1,25 @@
 const API_URL = 'https://www.themealdb.com';
 
 const xhr = new XMLHttpRequest();
+let data;
 
-function onRequestHandler() {
-    if(this.readyState === 4 && this.status === 200){
-        //0 = UNSET no se ha llamado al metodo open
-        //1 = OPENED , se ha llamado al metodo opened
-        //2 = HEADERS_RECEIVED, se está llamando al metodo send()
-        //3 = LOADING, esta cargando, es decir, esta recibiendo la respuesta
-        //4 = DONE , se ha completado la operacion del
-        console.log(this.response);
-        const data = Object.entries(JSON.parse(this.response));
-        console.log(Array.isArray(data));
-        console.log(data);
-        const HTMLResponse = document.querySelector("#app");
+// function onRequestHandler() {
+//     if(this.readyState === 4 && this.status === 200){
+//         //0 = UNSET no se ha llamado al metodo open
+//         //1 = OPENED , se ha llamado al metodo opened
+//         //2 = HEADERS_RECEIVED, se está llamando al metodo send()
+//         //3 = LOADING, esta cargando, es decir, esta recibiendo la respuesta
+//         //4 = DONE , se ha completado la operacion del
+//         console.log(this.response);
+//         data = Object.entries(JSON.parse(this.response));
+//         console.log(Array.isArray(data));
+//         console.log(data);
+//         const HTMLResponse = document.querySelector("#app");
 
-        const template = data.map((meals) => `<li>${meals.strMeal}</li>`);
-        HTMLResponse.innerHTML = `<ul>${template}</ul>`;
-    }
-}
+//         const template = data.map((meals) => `<li>${meals.strMeal}</li>`);
+//         HTMLResponse.innerHTML = `<ul>${template}</ul>`;
+//     }
+// }
 
 function apiByName(nameSearch) {
     const xhrequest = new XMLHttpRequest();
@@ -30,11 +31,11 @@ function apiByName(nameSearch) {
         //2 = HEADERS_RECEIVED, se está llamando al metodo send()
         //3 = LOADING, esta cargando, es decir, esta recibiendo la respuesta
         //4 = DONE , se ha completado la operacion del
-        const data = Object.entries(JSON.parse(this.response));
+        data = Object.entries(JSON.parse(this.response));
         const template = data.map((meals) => `<li>${meals.strMeal}</li>`);
         console.log("Array: ",Array.isArray(data));
         console.log("data:", data);
-
+    
     }
     });
     xhrequest.open("GET",`${API_URL}/api/json/v1/1/search.php?s=${nameSearch}`);
@@ -44,3 +45,11 @@ function apiByName(nameSearch) {
 xhr.addEventListener("load", onRequestHandler);
 xhr.open("GET",`${API_URL}/api/json/v1/1/random.php`);
 xhr.send();*/
+console.log("nueva linea de texto, data antes de buscar: ", data);
+
+const printResults = document.getElementById("printResults");
+function print(){
+    console.log("function print is running");
+    console.log(data);
+}
+printResults.addEventListener("click", print);
