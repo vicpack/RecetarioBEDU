6e4f2164b076607508b753c94c9c50b7e7f07d4b
Dionicio <dioniciomatamacias@gmail.com>
2022-08-19 15:55:20 -0500

    div #app, variables dataHead dataMeals, imprime primer nombre

--- src/js/API.js
+++ src/js/API.js
@@ -32,10 +32,10 @@ function apiByName(nameSearch) {
         //3 = LOADING, esta cargando, es decir, esta recibiendo la respuesta
         //4 = DONE , se ha completado la operacion del
         data = Object.entries(JSON.parse(this.response));
-        const template = data.map((meals) => `<li>${meals.strMeal}</li>`);
+        // const template = data.map((meals) => `<li>${meals.strMeal}</li>`);
         console.log("Array: ",Array.isArray(data));
         console.log("data:", data);
-    
+
     }
     });
     xhrequest.open("GET",`${API_URL}/api/json/v1/1/search.php?s=${nameSearch}`);
@@ -51,5 +51,19 @@ const printResults = document.getElementById("printResults");
 function print(){
     console.log("function print is running");
     console.log(data);
-}
+
+        // Creé las variables dataHead y dataMeals para acceder a elementos del array
+        const dataHead = data[0][0]; 
+        const dataMeals = data[0][1];
+        console.log(dataHead); // logs "meals" 
+        console.log(dataMeals); // logs an array with 25 meal objects
+
+        //Imprimir el nombre del primer platillo al HTML
+        const HTMLResponse = document.querySelector("#app");
+        HTMLResponse.innerHTML = `<ul></ul>`;
+        const template = data.map((meals) => `<li>${meals.strMeal}</li>`);
+        HTMLResponse.firstChild.innerHTML = `<li>${dataMeals[0].strMeal}</li>`;
+        console.log(template.toString())
+  
+    }
 printResults.addEventListener("click", print);
