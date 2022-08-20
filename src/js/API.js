ddb7dd302461731206fc2aedf1e0a471b3aacefb
dioniciomata <101280667+dioniciomata@users.noreply.github.com>
2022-08-19 21:52:53 -0500

    Nueva rama dev/dio (#7)
    * Agregar botón 'Print Results' al front-end
    
    * en archivo API.js expuse la variable 'data' al scope global, para luego poder imprimirla con 'function print'
    
    * div #app, variables dataHead dataMeals, imprime primer nombre
    
    * agrege un for of, ya se pintan las imágenes de las recetas dentro de cards
    
    * working on card-body
    
    * conflictos entre ramas resueltos
    
    * comentarios agregados en for(meal of meals)

--- src/js/API.js
+++ src/js/API.js
@@ -1,24 +1,25 @@
 const API_URL = 'https://www.themealdb.com';
 
 const xhr = new XMLHttpRequest();
+let data;
 
-function onRequestHandler() {
-    if(this.readyState === 4 && this.status === 200){
-        //0 = UNSET no se ha llamado al metodo open
-        //1 = OPENED , se ha llamado al metodo opened
-        //2 = HEADERS_RECEIVED, se está llamando al metodo send()
-        //3 = LOADING, esta cargando, es decir, esta recibiendo la respuesta
-        //4 = DONE , se ha completado la operacion del
-        console.log(this.response);
-        const data = Object.entries(JSON.parse(this.response));
-        console.log(Array.isArray(data));
-        console.log(data);
-        const HTMLResponse = document.querySelector("#app");
+// function onRequestHandler() {
+//     if(this.readyState === 4 && this.status === 200){
+//         //0 = UNSET no se ha llamado al metodo open
+//         //1 = OPENED , se ha llamado al metodo opened
+//         //2 = HEADERS_RECEIVED, se está llamando al metodo send()
+//         //3 = LOADING, esta cargando, es decir, esta recibiendo la respuesta
+//         //4 = DONE , se ha completado la operacion del
+//         console.log(this.response);
+//         data = Object.entries(JSON.parse(this.response));
+//         console.log(Array.isArray(data));
+//         console.log(data);
+//         const HTMLResponse = document.querySelector("#app");
 
-        const template = data.map((meals) => `<li>${meals.strMeal}</li>`);
-        HTMLResponse.innerHTML = `<ul>${template}</ul>`;
-    }
-}
+//         const template = data.map((meals) => `<li>${meals.strMeal}</li>`);
+//         HTMLResponse.innerHTML = `<ul>${template}</ul>`;
+//     }
+// }
 
 function apiByName(nameSearch) {
     const xhrequest = new XMLHttpRequest();
@@ -30,8 +31,8 @@ function apiByName(nameSearch) {
         //2 = HEADERS_RECEIVED, se está llamando al metodo send()
         //3 = LOADING, esta cargando, es decir, esta recibiendo la respuesta
         //4 = DONE , se ha completado la operacion del
-        const data = Object.entries(JSON.parse(this.response));
-        const template = data.map((meals) => `<li>${meals.strMeal}</li>`);
+        data = Object.entries(JSON.parse(this.response));
+        // const template = data.map((meals) => `<li>${meals.strMeal}</li>`);
         console.log("Array: ",Array.isArray(data));
         console.log("data:", data);
 
@@ -44,3 +45,58 @@ function apiByName(nameSearch) {
 xhr.addEventListener("load", onRequestHandler);
 xhr.open("GET",`${API_URL}/api/json/v1/1/random.php`);
 xhr.send();*/
+console.log("nueva linea de texto, data antes de buscar: ", data);
+
+const printResults = document.getElementById("printResults");
+function print(){
+    console.log("function print is running");
+    console.log(data);
+
+        // Creé las variables dataHead y dataMeals para acceder a elementos del array
+        const dataHead = data[0][0]; 
+        const dataMeals = data[0][1];
+        console.log(dataHead); // logs "meals" 
+        console.log(dataMeals); // logs an array with 25 meal objects
+
+        //Imprimir el nombre del primer platillo al HTML
+        const HTMLResponse = document.querySelector("#app");
+        // HTMLResponse.innerHTML = `<ul></ul>`;
+        // const template = data.map((meals) => `<li>${meals.strMeal}</li>`);
+        // HTMLResponse.firstChild.innerHTML = `<li>${dataMeals[0].strMeal}</li>`;
+        
+        // Cree la function createNode
+        function createNode(type, child){
+            const node = document.createElement(type);
+            if (typeof child === "string"){
+             const text = document.createTextNode(child);
+             node.appendChild(text);
+            } else {
+             node.appendChild(child);
+            }
+            return node;
+        }
+
+        //Imprimir un card a partir de cada elemento del array
+        for (const meal of dataMeals) {
+            const col = document.createElement("div");
+            col.className="col";
+
+            // Add <div class="card shadow-sm">
+            const  card = document.createElement("div");
+            card.className =  "card shadow-sm";
+            col.appendChild(card);
+
+            //Add img with class "bd-placeholder-img card-img-top"
+            const img = document.createElement("img");
+            img.className = "bd-placeholder-img card-img-top";
+            // img.height = "225";
+            img.role="img";
+            img.src=""+meal.strMealThumb;
+            card.appendChild(img);
+
+            console.log(col)
+            HTMLResponse.appendChild(col);
+          }
+  
+    }
+printResults.addEventListener("click", print);
