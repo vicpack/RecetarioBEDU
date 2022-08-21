11cc867f9d48ca0755af3a3162a3a7e96350f02a
vicpack <vicmoze95@gmail.com>
2022-08-20 20:49:25 -0500

    Arreglando el bug de que no aparecian las imagenes, faltaba cerrar comillas en la class del segundo div

--- src/js/apiMeal.js
+++ src/js/apiMeal.js
@@ -7,16 +7,18 @@ getMealBtn.addEventListener('click',() =>{
         .then(res => {
                /*lo colocamos en la posición 0 por que la API
             viene solo con un objeto */
-            createMeal(res.meals[0])
-        })
+            console.log('res');
+            createMeal(res.meals[0]);
+        });
 });
 
 function createMeal(meal) {
     mealContainer.innerHTML = `
         <div class="row">
-            <div class="column five>
+            <div class="column five">
                 <img src="${meal.strMealThumb}" alt="Meal Img" />
             </div>
         </div>
     `;
+    console.log(meal.strMealThumb);
 }
