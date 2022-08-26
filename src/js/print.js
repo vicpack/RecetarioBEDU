c613f1cc73bba6c83d3d11568b487bac17d7e83e
dioniciomata <101280667+dioniciomata@users.noreply.github.com>
2022-08-26 18:24:57 -0500

    Si el input esta vacio, mensaje "No has ingresado nada" (#16)
    * HTML card con imagen, nombre, categoría y area
    
    * styles and image and API.dev added
    
    * move function print to new js file 'print.js'
    
    * la funcion ya se ejecuta con los ID correctos, y borré input y botón de arriba
    
    * Esperar a que se ejecute la funcion apiByName() antes de imprimir resultados
    
    * file change
    
    * agregué un mensaje'No has ingresado nada' cuando el input esta vacio

--- src/js/print.js
+++ src/js/print.js
@@ -35,128 +35,140 @@ function noResult(search){
 
 function print() {
   console.log("function print is running");
-  let searchBtn = inputByName.value.toUpperCase();
-  console.log("Buscar: ", searchBtn)
-  apiByName(searchBtn);
-  console.log(data);
 
-  // Esperar a que se ejecute la funcion apiByName()
-  setTimeout(function () {
-    // Creé las variables dataHead y dataMeals para acceder a elementos del array
-    const dataHead = data[0][0];
-    const dataMeals = data[0][1];
-    console.log(dataHead); // logs "meals" 
-    console.log(dataMeals); // logs an array with 25 meal objects
+    if (inputByName.value) {
+      console.log("input is true")
+      let searchBtn = inputByName.value.toUpperCase();
+    console.log("Buscar: ", searchBtn)
+    apiByName(searchBtn);
+    console.log(data);
+
+    // Esperar a que se ejecute la funcion apiByName()
+    setTimeout(function () {
+      // Creé las variables dataHead y dataMeals para acceder a elementos del array
+      const dataHead = data[0][0];
+      const dataMeals = data[0][1];
+      console.log(dataHead); // logs "meals" 
+      console.log(dataMeals); // logs an array with 25 meal objects
 
 
-    // Crear el node divCardsResult <section id="indexResultados" class="row row-cols-1 row-cols-md-3 g-4 mt-4">
-    
+      // Crear el node divCardsResult <section id="indexResultados" class="row row-cols-1 row-cols-md-3 g-4 mt-4">
+      
 
-    //Imprimir el nombre del primer platillo al HTML
-    // const HTMLResponse = document.querySelector("#app");
-    // HTMLResponse.innerHTML = `<ul></ul>`;
-    // const template = data.map((meals) => `<li>${meals.strMeal}</li>`);
-    // HTMLResponse.firstChild.innerHTML = `<li>${dataMeals[0].strMeal}</li>`;
+      //Imprimir el nombre del primer platillo al HTML
+      // const HTMLResponse = document.querySelector("#app");
+      // HTMLResponse.innerHTML = `<ul></ul>`;
+      // const template = data.map((meals) => `<li>${meals.strMeal}</li>`);
+      // HTMLResponse.firstChild.innerHTML = `<li>${dataMeals[0].strMeal}</li>`;
 
-    // Cree la function createNode
-    function createNode(type, child) {
-      const node = document.createElement(type);
-      if (typeof child === "string") {
-        const text = document.createTextNode(child);
-        node.appendChild(text);
-      } else {
-        node.appendChild(child);
-      }
-      return node;
-    }
-
-    if (!dataMeals) {
-      console.log("dataMeals is null");
-      noResult(searchBtn);
-
-      // const noResults = document.createElement("p");
-      // const text = document.createTextNode("No hay resultados");
-      // noResults.appendChild(text);
-      // divCardsResult.appendChild(noResults);
-
-      // HTMLResponse.replaceChild(divCardsResult, HTMLResponse.childNodes[0]);
-    }
-    //Imprimir un card a partir de cada elemento del array
-    // <div class="col">
-    //     <div class="card h-100">
-    //         <img src="..." class="card-img-top" alt="...">
-    //         <div class="card-body">
-    //             <h5 class="card-title">Card title</h5>
-    //             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional
-    //             content. This content is a little bit longer.</p>
-    //         </div>
-    //     </div>
-    // </div>
-    console.log(Array.isArray(dataMeals))
-    if (Array.isArray(dataMeals)) {
-      cleanResults()
-      showResult(dataMeals.length);
-      for (const meal of dataMeals) {
-        const col = document.createElement("div");
-        col.className = "col";
-
-        // Add <div class="card shadow-sm">
-        const card = document.createElement("div");
-        card.className = "card h-100";
-        col.appendChild(card);
-
-        //Add img with class "bd-placeholder-img card-img-top"
-        const img = document.createElement("img");
-        img.className = "bd-placeholder-img card-img-top";
-        // img.height = "225";
-        img.alt = "" + meal.strMeal;
-        img.role = "img";
-        img.src = "" + meal.strMealThumb;
-        card.appendChild(img);
-
-        // <div class="card-body"> 
-        const cardBody = document.createElement("div");
-        cardBody.className = "card-body";
-        card.appendChild(cardBody);
-
-        // Add <h5 class="card-title">Card title</h5>
-        const cardMeal = createNode("h5", meal.strMeal);
-        cardMeal.className = "card-title";
-        cardBody.appendChild(cardMeal);
-
-        // Add <p class="card-text"> con strMeal strCategory y strArea
-        const cardCategory = createNode("p", meal.strCategory);
-        cardBody.appendChild(cardCategory);
-        const cardArea = createNode("p", meal.strArea);
-        cardArea.className = "card-text";
-        cardBody.appendChild(cardArea);
-
-        // Add <div class="d-flex justify-content-between align-items-center">
-        const botones = document.createElement("div");
-        botones.className = "d-flex justify-content-between align-items-center";
-        cardBody.appendChild(botones);
-
-        // Add <div class="btn-group">
-        const btnGroup = document.createElement("div");
-        btnGroup.className = "btn-group";
-        botones.appendChild(btnGroup);
-
-        // <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-        const viewButton = createNode("button", "Ver detalles");
-        const secButton = createNode("button", "Otro botón");
-        viewButton.className = "btn btn-sm btn-outline-secondary";
-        secButton.className = "btn btn-sm btn-outline-secondary";
-        btnGroup.appendChild(viewButton);
-        btnGroup.appendChild(secButton);
-
-        console.log(col)
-        divCardsResult.appendChild(col);
+      // Cree la function createNode
+      function createNode(type, child) {
+        const node = document.createElement(type);
+        if (typeof child === "string") {
+          const text = document.createTextNode(child);
+          node.appendChild(text);
+        } else {
+          node.appendChild(child);
+        }
+        return node;
       }
 
-      // Reemplaza los resultados anteriores con los resultados de búsqqueda nuevos
-      // HTMLResponse.replaceChild(divCardsResult, HTMLResponse.childNodes[0]);
-    }
-  }, 500);
+      if (!dataMeals) {
+        console.log("dataMeals is null");
+        noResult(searchBtn);
+
+        // const noResults = document.createElement("p");
+        // const text = document.createTextNode("No hay resultados");
+        // noResults.appendChild(text);
+        // divCardsResult.appendChild(noResults);
+
+        // HTMLResponse.replaceChild(divCardsResult, HTMLResponse.childNodes[0]);
+      }
+      //Imprimir un card a partir de cada elemento del array
+      // <div class="col">
+      //     <div class="card h-100">
+      //         <img src="..." class="card-img-top" alt="...">
+      //         <div class="card-body">
+      //             <h5 class="card-title">Card title</h5>
+      //             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional
+      //             content. This content is a little bit longer.</p>
+      //         </div>
+      //     </div>
+      // </div>
+      console.log(Array.isArray(dataMeals))
+      if (Array.isArray(dataMeals)) {
+        cleanResults()
+        showResult(dataMeals.length);
+        for (const meal of dataMeals) {
+          const col = document.createElement("div");
+          col.className = "col";
+
+          // Add <div class="card shadow-sm">
+          const card = document.createElement("div");
+          card.className = "card h-100";
+          col.appendChild(card);
+
+          //Add img with class "bd-placeholder-img card-img-top"
+          const img = document.createElement("img");
+          img.className = "bd-placeholder-img card-img-top";
+          // img.height = "225";
+          img.alt = "" + meal.strMeal;
+          img.role = "img";
+          img.src = "" + meal.strMealThumb;
+          card.appendChild(img);
+
+          // <div class="card-body"> 
+          const cardBody = document.createElement("div");
+          cardBody.className = "card-body";
+          card.appendChild(cardBody);
+
+          // Add <h5 class="card-title">Card title</h5>
+          const cardMeal = createNode("h5", meal.strMeal);
+          cardMeal.className = "card-title";
+          cardBody.appendChild(cardMeal);
+
+          // Add <p class="card-text"> con strMeal strCategory y strArea
+          const cardCategory = createNode("p", meal.strCategory);
+          cardBody.appendChild(cardCategory);
+          const cardArea = createNode("p", meal.strArea);
+          cardArea.className = "card-text";
+          cardBody.appendChild(cardArea);
+
+          // Add <div class="d-flex justify-content-between align-items-center">
+          const botones = document.createElement("div");
+          botones.className = "d-flex justify-content-between align-items-center";
+          cardBody.appendChild(botones);
+
+          // Add <div class="btn-group">
+          const btnGroup = document.createElement("div");
+          btnGroup.className = "btn-group";
+          botones.appendChild(btnGroup);
+
+          // <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+          const viewButton = createNode("button", "Ver detalles");
+          const secButton = createNode("button", "Otro botón");
+          viewButton.className = "btn btn-sm btn-outline-secondary";
+          secButton.className = "btn btn-sm btn-outline-secondary";
+          btnGroup.appendChild(viewButton);
+          btnGroup.appendChild(secButton);
+
+          console.log(col)
+          divCardsResult.appendChild(col);
+        }
+
+        // Reemplaza los resultados anteriores con los resultados de búsqqueda nuevos
+        // HTMLResponse.replaceChild(divCardsResult, HTMLResponse.childNodes[0]);
+      }
+    }, 500);
+  } else {
+    console.log("input is empty");
+    const emptyTag = document.createElement("p");
+    const emptyText = document.createTextNode("No has ingresado nada");
+    emptyTag.appendChild(emptyText);
+    if (divCardsResult.firstChild){
+      divCardsResult.replaceChild(emptyTag, divCardsResult.firstChild);
+    } else {divCardsResult.appendChild(emptyTag)};
+  }  
 }
 
 searchByName.addEventListener("click", print);
