bfa909f8eb24f34cbf280670898fa51289b5e7d2
vicpack <vicmoze95@gmail.com>
2022-08-29 15:37:49 -0500

    Victor (#20)
    * agregando html generador a random.html
    
    * Agregando función de al cargar página se muestre receta random
    
    * Limpiando variables y funciones que ya no usamos
    
    * Agregando estilo al random
    
    * terminando el estilo para la card de imagen,area,categoriay tag
    
    * ya alineados los divs
    
    * ajustando solo falta el card bonito para video y las intrucciones
    
    * Agregando cards para instrucciones y estilo a letras
    
    * Finalizando card para video y estilos para fuentes
    
    * Agregando funcion que regresa al index en el boton like a chef y corrigiendo el index.html
    
    * agregando responsive
    
    Co-authored-by: dioniciomata <101280667+dioniciomata@users.noreply.github.com>

--- src/js/apiMeal.js
+++ src/js/apiMeal.js
@@ -30,54 +30,59 @@ function createMeal(meal) {
   console.log(ingredients);
 
   mealContainer.innerHTML = `
-        <div class="card-body" id="contenedor">
-            <div class="card border-0 shadow cursor" style="width: 18rem; border-radius: 10px">
-                <img class="card-img-top" style="width: 18rem; border-radius: 10px 10px 0 0" src="${
-                  meal.strMealThumb
-                }" alt="Meal Img" />
-                <div class="card-body">
-                    <p class="card-text"><strong>Category: </strong>${
-                      meal.strCategory
-                    }</p>
-                    <p class="card-text"><strong>Area: </strong>${
-                      meal.strArea
-                    }</p>
-                    <p class="card-text"><strong>Tags: </strong>${meal.strTags
-                      .split(",")
-                      .join(", ")}</p>
-                </div>
-                <br>
-                <div class="card-body">
-                <h5 class="card-title">Ingredients</h5>
-                <ul class="list-group list-group-flush">
-                    ${ingredients
-                      .map(
-                        (ingredient) =>
-                          `<li class="list-group-item">${ingredient}</li>`
-                      )
-                      .join("")}
-                </ul>
-                </div>
-            </div>
-              <div class="col-md-8">
+  <div class="card-body" id="contenedor">
+  <div class="row g-3">
+      <div class="col-12 col-md-6 col-lg-4">
+          <div class="border-0 shadow cursor" style="width: 18rem; border-radius: 10px">
+              <img class="card-img-top" style="width: 18rem; border-radius: 10px 10px 0 0" src="${
+                meal.strMealThumb
+              }" alt="Meal Img" />
+              <div class="card-body">
+                  <p class="card-text"><strong>Category: </strong>${
+                    meal.strCategory
+                  }</p>
+                  <p class="card-text"><strong>Area: </strong>${
+                    meal.strArea
+                  }</p>
+                  <p class="card-text"><strong>Tags: </strong>${meal.strTags
+                    .split(",")
+                    .join(", ")}</p>
+              </div>
+              <br>
+              <div class="card-body">
+                  <h5 class="card-title">Ingredients</h5>
+                  <ul class="list-group list-group-flush">
+                      ${ingredients
+                        .map(
+                          (ingredient) =>
+                            `<li class="list-group-item">${ingredient}</li>`
+                        )
+                        .join("")}
+                  </ul>
+              </div>
+          </div>
+      </div>
+      <div class="col-12 col-md-6 col-lg-4">
+          <div class="col-md-8">
               <div class="card border-0 shadow cursor" style="width: 55rem; border-radius: 10px">
                   <h4 class="card-title text-center">${meal.strMeal}</h4>
                   <br>
                   <p>${meal.strInstructions}</p>
               </div>
               <br>
-                  <div class="card border-0 shadow cursor" style="width: 55rem; border-radius: 10px">
-                        <br>
-                    <h4 class="card-title text-center">Meal video</h4>
-                        <div class="videoWrapper" style="width: 55rem; border-radius: 10px">
-                          <iframe src="https://www.youtube.com/embed/${meal.strYoutube.slice(
-                            -11
-                          )}" />
-                        </div>
+              <div class="card border-0 shadow cursor" style="width: 55rem; border-radius: 10px">
+                  <br>
+                  <h4 class="card-title text-center">Meal video</h4>
+                  <div class="videoWrapper" style="width: 55rem; border-radius: 10px">
+                      <iframe src="https://www.youtube.com/embed/${meal.strYoutube.slice(
+                        -11
+                      )}" />
                   </div>
               </div>
-        </div>
-       
+          </div>
+      </div>
+  </div>
+</div>
     `;
   console.log(meal.strMealThumb);
 }
