d39ba21190582012052ec98588d33e18be9b2671
Dionicio <dioniciomatamacias@gmail.com>
2022-08-31 18:57:28 -0500

    entregar ID a pagina nueva

--- src/js/print.js
+++ src/js/print.js
@@ -77,9 +77,6 @@ function createNode(type, child) {
 // Crear tarjetas para imprimir los resultados
 function createCards(){
 
-    // Limpia el buscador despues de buscar una comida
-    const mealDetailsSection = document.getElementById('meal-details-section');
-    mealDetailsSection.innerHTML = "";
 
     // Creé las variables dataHead y dataMeals para acceder a elementos del array
     const dataHead = data[0][0];
@@ -172,12 +169,6 @@ function createCards(){
         btnGroup.className = "btn-group";
         botones.appendChild(btnGroup);
 
-        // <a href="#meal-details-section" style="text-decoration: none;">
-        const ahref = document.createElement("a");
-        ahref.href="#meal-details-section";
-        ahref.style="text-decoration: none;";
-        btnGroup.appendChild(ahref);
-
         // <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
         // const viewButton = createNode("button", "Ver detalles");
         // const secButton = createNode("button", "Otro botón");
@@ -188,7 +179,7 @@ function createCards(){
         const viewInfo = `
         <button onclick="getMealInfo(${meal.idMeal})" type="button" class="btn btn-sm btn-outline-secondary">Ver detalles</button>
             `
-        ahref.innerHTML = viewInfo;
+        btnGroup.innerHTML = viewInfo;
         // const mealInfoDiv = document.createElement('div');
         // mealInfoDiv.className = 'col-xm-1 col-sm-1 col-md-3 p-3 d-flex justify-content-center';
         // mealInfoDiv.innerHTML = mealInfo;
@@ -223,10 +214,6 @@ function print() {
     console.log("input is empty");
     cleanResults();
 
-    // Limpia el buscador despues de buscar una comida
-    const mealDetailsSection = document.getElementById('meal-details-section');
-    mealDetailsSection.innerHTML = "";
-
     const emptyTag = document.createElement("p");
     const emptyText = document.createTextNode("No has ingresado nada");
     emptyTag.appendChild(emptyText);
@@ -243,56 +230,11 @@ inputByName.addEventListener("keypress", function (event) {
 });
 
 function getMealInfo (mealID) {
-  // Limpia el buscador despues de buscar una comida
 
-
-
-  const mealDetailsSection = document.getElementById('meal-details-section');
-  mealDetailsSection.innerHTML = "";
-
-  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`
+  
+      window.location.href = `/src/app/receta.html?i=${mealID}`; // regresa la URL mostrando el ID de la receta
+      const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`
   fetch(url)
       .then(res => res.json())
-      .then(data => showMealDetailsDiv(data));
+      .then(data => console.log(data));
 } 
-
-// Mostrar detalles de la comida 
-
-const showMealDetailsDiv = data => {
-    const meal = data.meals[0];
-    const mealPhoto = meal.strMealThumb;
-    const mealName = meal.strMeal;
-    const mealInstructions = meal.strInstructions;
-    window.location.href += `/${meal.idMeal}`; // regresa la URL mostrando el ID de la receta
-
-    console.log(meal.strInstructions)
-
-
-    // Establecer estructura de división de detalles de comidas
-    const mealDetailsSection = document.getElementById('meal-details-section');
-    mealDetailsSection.innerHTML = ` 
-        <div id="meal-details" class="card px-0 pb-1 border-0 shadow col-xm-12 col-sm-12 col-md-6" style="border-radius: 10px;">
-            <img src="${mealPhoto}" class="card-img-top" style="border-radius: 10px 10px 0 0;" alt=" ...">
-            <div class="card-body">
-                <h2 class="card-title text-center my-3">${mealName}</h2>
-                <p class= "card-instruc text-center my-3">${mealInstructions}</p>
-                <hr>
-                <h5 class="card-title mt-4">Meal Ingredients</h5>
-                <div id="meal-ingredients"></div>
-            </div>
-        </div>
-    `
-
-    const mealIngredients = document.getElementById('meal-ingredients');
-
-    // // Establecer el contenido de cada párrafo dentro de la estructura de la división de detalles de la comida
-    for (let i = 1; meal[`strIngredient${i}`]; i++) {
-        const ingredients = `
-        ✔ ${meal[`strMeasure${i}`]} ${meal[`strIngredient${i}`]} 
-        `
-        const mealDetailsP = document.createElement('p');
-        mealDetailsP.className = 'card-text';
-        mealDetailsP.innerText = ingredients;
-        mealIngredients.appendChild(mealDetailsP);
-    }
-}
