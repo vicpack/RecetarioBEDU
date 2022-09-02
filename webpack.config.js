db3118226148bf935b6fcfd97709640c6dd4be36
Dionicio <dioniciomatamacias@gmail.com>
2022-09-01 23:00:36 -0500

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
-      bundle: './src/js/API.js'
+      bundle: './src/js/apiMeal.js'
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
